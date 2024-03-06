86327465d15862987cfe10297a49a2d08ba9027a
Prince Gaurang Desai <princedesai7729@gmail.com>
2024-03-05 20:11:04 -0500

    Added 5 test cases for scalene triangle

--- TestTriangle/TestTriangle.cs
+++ TestTriangle/TestTriangle.cs
@@ -9,7 +9,7 @@ namespace TestTriangle
         public class Testing
         {
             [Test]
-            public void checkForEquailateralTriangleInput5and5and5OutputEquilateraltriangle()
+            public void checkForValidEquailateralTriangle_Input5and5and5_OutputEquilateraltriangle()
             {
                 int firstSide = 5;
                 int secondSide = 5;
@@ -19,7 +19,7 @@ namespace TestTriangle
                 Assert.That(actual, Is.EqualTo(expected));
             }
             [Test]
-            public void checkValidIso1()
+            public void checkForValidIsoscelesTriangle_Input5and4and5_OutputIsoscelestriangle()
             {
                 int firstSide = 5;
                 int secondSide = 4;
@@ -29,7 +29,7 @@ namespace TestTriangle
                 Assert.That(actual, Is.EqualTo(expected));
             }
             [Test]
-            public void checkValidIso2() 
+            public void checkForValidIsoscelesTriangle_Input5and5and7_OutputIsoscelestriangle()
             {
                 int firstSide = 5;
                 int secondSide = 5;
@@ -39,13 +39,62 @@ namespace TestTriangle
                 Assert.That(actual, Is.EqualTo(expected));
             }
             [Test]
-            public void checkValidIso3() 
+            public void checkForValidIsoscelesTriangle_Input3and5and5_OutputIsoscelestriangle()
             {
                 int firstSide = 3;
                 int secondSide = 5;
                 int thirdSide = 5;
                 string expected = "Isosceles triangle";
                 string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+                Assert.That(actual, Is.EqualTo(expected));
+            }
+            [Test]
+            public void checkForValidScalene_Input3and4and5_OutputScalene()
+            {
+                int firstSide = 3;
+                int secondSide = 4;
+                int thirdSide = 5;
+                string expected = "Scalene triangle";
+                string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+                Assert.That(actual, Is.EqualTo(expected));
+            }
+            [Test]
+            public void checkForValidScalene_Input7and10and12_OutputScalene()
+            {
+                int firstSide = 7;
+                int secondSide = 10;
+                int thirdSide = 12;
+                string expected = "Scalene triangle";
+                string actual=Triangle.AnalyzeTriangle(firstSide,secondSide, thirdSide);
+                Assert.That(actual,Is.EqualTo(expected));
+            }
+            [Test]
+            public void checkForScalene_Input5and9and13_OutputScalene()
+            {
+                int firstSide = 5;
+                int secondSide = 9;
+                int thirdSide = 13;
+                string expected = "Scalene triangle";
+                string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+                Assert.That(actual , Is.EqualTo(expected));
+            }
+            [Test]
+            public void checkForScalene_Input6and8and10_OutputScalene()
+            {
+                int firstSide = 6;
+                int secondSide = 8;
+                int thirdSide = 10;
+                string expected = "Scalene triangle";
+                string actual=Triangle.AnalyzeTriangle(firstSide,secondSide,thirdSide);
+                Assert.That (actual, Is.EqualTo(expected));
+            }
+            public void checkForScalene_Input15and20and25_OutputScalene()
+            {
+                int firstSide = 15;
+                int secondSide = 20;
+                int thirdSide = 25;
+                string expected = "Scalene triangle";
+                string actual=Triangle.AnalyzeTriangle(firstSide,secondSide, thirdSide);
                 Assert.That(actual, Is.EqualTo(expected));
             }
         }
