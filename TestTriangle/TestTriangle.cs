13d779d3bf59c42a81bdaff9610bbe85679e6630
Prince Gaurang Desai <princedesai7729@gmail.com>
2024-03-05 19:34:27 -0500

    Testcase for equilateral triangle. Also added the required package Microsoft.Net.Test.Sdk

--- TestTriangle/TestTriangle.cs
+++ TestTriangle/TestTriangle.cs
@@ -1,7 +1,24 @@
-﻿namespace TestTriangle
+﻿using NUnit.Framework;
+using TriangleSolver;
+
+namespace TestTriangle
 {
     public class TestTriangle
     {
+        [TestFixture]
+        public class Testing
+        {
+            [Test]
+            public void checkForEquailateralTriangleInput5and5and5OutputEquilateraltriangle()
+            {
+                int firstSide = 5;
+                int secondSide = 5;
+                int thirdSide = 5;
+                string expected = "Equilateral triangle";
+                string actual=Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+                Assert.That(actual, Is.EqualTo(expected));
+            }
+        }
 
     }
 }
